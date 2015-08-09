14c290202ef1267a4ac1e8a1daf41f587daba3fe
joe <18810544291@163.com>
2015-08-09 11:57:14 +0800

    基本完成，就是上下滑在手机上不好用

--- app/js/summer/task2.js
+++ app/js/summer/task2.js
@@ -1,20 +1,56 @@
 $(function () {
-	var handler;
-	$('.slide').css({'height': window.document.documentElement.clientHeight}).first().addClass('curr');
-	window.addEventListener('resize', function () {
+	var handler, height, startY, baseY;
+	$(window).on('resize, load', function () {
 		clearTimeout(handler);
 		handler = setTimeout(function () {
-			$('.slide').css({'height': window.document.documentElement.clientHeight});
+			height = window.document.documentElement.clientHeight;
+			$('#wrapper, .slide').css({'height': height + 'px'});
 		}, 300);
 	}, false);
-	$('.arrow').on('tap', function () {
-		var index = $('.curr').index();
-		var l = $('.slide').length;
-		if (index < l - 1)  {
-			$('.slide').removeClass('curr').eq(index + 1).addClass('curr');
-			if (index  === l - 2) {
-				$('.arrow').css('backgroundColor', 'gray');
-			}
-		}
+
+	var slides = $('.slides');
+	var arrow = $('.ascend, .descend');
+	arrow.on('click, touchstart', function () {
+		swipe(this.className === 'ascend'? 'up' : 'down');
 	});
+	slides.on('touchstart', function (e) {
+		startY = e.changedTouches[0].clientY;
+		baseY = parseFloat(slides.css('transform').match(/translateY\((.*)px\)/)[1]);
+	})
+	slides.on('touchmove', function (e) {
+		var moveY = e.changedTouches[0].clientY;
+		var offset = moveY - startY;
+		move(offset);
+	})
+	slides.on('touchend', function (e) {
+		var endY = e.changedTouches[0].clientY;
+		var offset = endY - startY;
+		move(offset < 0 ? -height : height, true);
+	});
+	function move(offset, isAnimate) {
+		var y = baseY + offset;
+		if (isAnimate) {
+			slides.animate({
+				'transform': 'translateY(' + y + 'px)', 
+				'-webkit-transform': 'translateY(' + y + 'px)'
+			}, 
+			{
+				duration: 200
+			})
+		}
+		else {
+			slides.css({
+				'transform': 'translateY(' + y + 'px)', 
+				'-webkit-transform': 'translateY(' + y + 'px)'
+			})
+		}
+	}
+	function swipe(dir) {
+		var currY = parseFloat(slides.css('transform').match(/translateY\((.*)px\)/)[1]);
+		var y = dir === 'up' ? currY - height : currY + height;
+		slides.css({
+			'transform': 'translateY(' + y + 'px)', 
+			'-webkit-transform': 'translateY(' + y + 'px)'
+		})
+	}
 })
--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,25 @@
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 var browserSync = require('browser-sync');
-
+var autoprefixer = require('gulp-autoprefixer');
 gulp.task('serve', function () {
 	browserSync({
 		files: '**',
 		server: {
 			baseDir: 'app',
 			routes: {
-				'/bower_components': 'bower_components'
+				'/bower_components': 'bower_components',
+				'/dist': 'dist'
 			}
 		}
 	});
+	gulp.watch('app/css/summer/**/*.css', ['prefixer'])
+});
+gulp.task('prefixer', function () {
+	return gulp.src('app/css/summer/**/*.css')
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions'],
+			cascade: false
+		}))
+		.pipe(gulp.dest('dist/css/summer/'));
 });
