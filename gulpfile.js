95372cb490b3374610ce601da793dcc4b1fa9bbd
joe <18810544291@163.com>
2015-08-09 09:02:24 +0800

    增加了gulp-inject和gulp-load-plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
 var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
 var browserSync = require('browser-sync');
 
 gulp.task('serve', function () {
 	browserSync({
 		files: '**',
 		server: {
-			baseDir: 'app'
+			baseDir: 'app',
+			routes: {
+				'/bower_components': 'bower_components'
+			}
 		}
 	});
 });
