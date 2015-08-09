b40750d241bf7e35f768d05d3c337b83db32fb80
joe <18810544291@163.com>
2015-08-09 23:11:53 +0800

    不容易啊，终于做好了

--- app/js/summer/task2.js
+++ app/js/summer/task2.js
@@ -5,26 +5,32 @@ $(function () {
 		handler = setTimeout(function () {
 			height = window.document.documentElement.clientHeight;
 			$('#wrapper, .slide').css({'height': height + 'px'});
-			$('.slides').css({
-				'transform': 'translateY(0px)', 
-				'-webkit-transform': 'translateY(0px)'
-			});
+			$('.slides').css(prefixer('transform', 'translateY', 0));
 		}, 300);
 	}, false);
 	var slides = $('.slides');
-	$('.ascend, .descend').on('click, touchstart', function () {
+	$('.ascend, .descend').on('click', function () {
 		swipe(this.className === 'ascend'? 'up' : 'down');
 	});
-	$(window).on('touchstart', function (e) {
+	slides.on('touchstart', function (e) {
+		e.preventDefault();
+		var translate = slides.css('transform') || slides.css('-webkit-transform');
 		startY = e.changedTouches[0].clientY;
-		baseY = parseFloat(slides.css('transform').match(/translateY\((.*)px\)/)[1]);
+		baseY = parseFloat(translate.match(/translateY\((.*)px\)/)[1]);
 	})
 	slides.on('touchmove', function (e) {
+		e.preventDefault();
 		var moveY = e.changedTouches[0].clientY;
 		var offset = moveY - startY;
 		move(offset);
 	})
 	slides.on('touchend', function (e) {
+		e.preventDefault();
+		var endY = e.changedTouches[0].clientY;
+		var offset = endY - startY;
+		move(offset < 0 ? -height : height, true);
+	});
+	slides.on('touchcancel', function (e) {
 		var endY = e.changedTouches[0].clientY;
 		var offset = endY - startY;
 		move(offset < 0 ? -height : height, true);
@@ -35,34 +41,32 @@ $(function () {
 			if (y > 0 || y <= -height * $('.slide').length) {
 				y = baseY;
 			}
-			slides.animate({
-				'transform': 'translateY(' + y + 'px)', 
-				'-webkit-transform': 'translateY(' + y + 'px)'
-			}, 
+			slides.animate(prefixer('transform', 'translateY', y), 
 			{
 				duration: 200
 			})
 		}
 		else {
-			slides.css({
-				'transform': 'translateY(' + y + 'px)', 
-				'-webkit-transform': 'translateY(' + y + 'px)'
-			})
+			slides.css(prefixer('transform', 'translateY', y))
 		}
 	};
 	function swipe(dir) {
-		var currY = parseFloat(slides.css('transform').match(/translateY\((.*)px\)/)[1]);
+		var translate = slides.css('transform') || slides.css('-webkit-transform');
+		var currY = parseFloat(translate.match(/translateY\((.*)px\)/)[1]);
 		var y = dir === 'up' ? currY - height : currY + height;
 		if (y > 0 || y <= -height * $('.slide').length) {
 			return;
 		}
 		else {
-			slides.animate({
-				'transform': 'translateY(' + y + 'px)', 
-				'-webkit-transform': 'translateY(' + y + 'px)'
-			}, {
-				duration: 200
-			})
+			slides.animate(prefixer('transform', 'translateY', y), {duration: 200})
 		}
 	};
+	function prefixer(cssAttr, cssValue, value, unite) {
+		var unite = unite || 'px';
+		var ob = {};
+		['', '-ms-', '-moz-', '-webkit-', '-o-'].forEach(function (item) {
+			ob[item + cssAttr] = cssValue + '(' + value + '' + unite + ')';
+		});
+		return ob;
+	}
 })
