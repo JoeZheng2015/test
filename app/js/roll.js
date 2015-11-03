6624c76673dea43e2a5bfcb37025088633da7592
joe <18810544291@163.com>
2015-11-03 08:56:58 +0800

    增加淘宝的摇奖js

---
+++ app/js/roll.js
@@ -0,0 +1,43 @@
+window.$ = $;
+roll()
+function roll() {
+	$('.red .bg').click();
+	setTimeout(function() {
+		if ($('[clk*=":close"]')) {
+			$('[clk*=":close"]').click();
+			roll();
+		}
+		else {
+			setTimeout(function() {
+				$('[clk*=":close"]').click();
+				roll();
+			}, 6000);
+		}
+	},1000);
+}
+setInterval(function(){
+	if($('.btn.show.s2')){
+		$('.btn.show.s2').click();
+	}if($('.btn.show.s1')){
+		$('.btn.show.s1').click()
+	}
+}, 100)
+click();
+function click() {
+	if($('.btn.show.s2')){
+		$('.btn.show.s2').click();
+	}if($('.btn.show.s1')){
+		$('.btn.show.s1').click()
+	}
+	var num = Math.random() * 1000;
+	setTimeout(click, num);
+};
+window.$=$;
+hua();
+function hua() {
+	$('#J_LotteryBTN').click();
+	setTimeout(function() {
+		$('.J_Close').click();
+		hua();
+	}, 5000);
+}
