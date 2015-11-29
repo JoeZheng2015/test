dbdd05e49c19ee2621e6179155556c2d82452beb
joe <18810544291@163.com>
2015-11-29 12:04:03 +0800

    完成gulp的compressresume压缩简历的task

--- gulpfile.js
+++ gulpfile.js
@@ -14,7 +14,18 @@ gulp.task('serve', function () {
 	});
 });
 gulp.task('compressjs', function() {
-	return gulp.src('app/**/*.js')
+	return gulp.src('app/resume/**/*.js')
 		.pipe($.uglify())
-		.pipe(gulp.dest('dist'));
-})
+		.pipe(gulp.dest('dist/resume/'));
+});
+gulp.task('compresscss', function() {
+	return gulp.src('app/resume/**/*.css')
+		.pipe($.minifyCss())
+		.pipe(gulp.dest('dist/resume/'));
+});
+gulp.task('compresshtml', function() {
+	return gulp.src('app/resume/**/*.html')
+		.pipe($.minifyHtml({loose: true}))
+		.pipe(gulp.dest('dist/resume/'));
+});
+gulp.task('compressresume', ['compressjs', 'compresscss', 'compresshtml']);
