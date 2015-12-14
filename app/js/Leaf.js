9ada21de37af383bd357e2f1a9eb77cfd55620be
joe <18810544291@163.com>
2015-12-14 08:47:12 +0800

    解释完落叶的demo

--- app/js/Leaf.js
+++ app/js/Leaf.js
@@ -1,103 +1,72 @@
 
 // 定义动画中的落叶数量
-const NUMBER_OF_LEAVES = 20;
+const NUMBER_OF_LEAVES = 40;
 
-// 当页面加载完时调用的初始化函数
-function init()
-{
-    /* Get a reference to the element that will contain the leaves */
+// 初始化函数
+function init() {
     var container = document.getElementById('leafContainer');
-    /* Fill the empty container with new leaves */
-    for (var i = 0; i < NUMBER_OF_LEAVES; i++) 
-    {
+    for (var i = 0; i < NUMBER_OF_LEAVES; i++) {
         container.appendChild(createALeaf());
     }
 }
 
-
-/*
-    Receives the lowest and highest values of a range and
-    returns a random integer that falls within that range.
-*/
-function randomInteger(low, high)
-{
+// 返回在low和high之间的随机的整型
+function randomInteger(low, high) {
     return low + Math.floor(Math.random() * (high - low));
 }
 
-
-/*
-   Receives the lowest and highest values of a range and
-   returns a random float that falls within that range.
-*/
-function randomFloat(low, high)
-{
+// 返回low和high间随机的浮点
+function randomFloat(low, high) {
     return low + Math.random() * (high - low);
 }
 
-
-/*
-    Receives a number and returns its CSS pixel value.
-*/
-function pixelValue(value)
-{
+// 用来增加px后缀
+function pixelValue(value) {
     return value + 'px';
 }
 
-
-/*
-    Returns a duration value for the falling animation.
-*/
-
-function durationValue(value)
-{
+// 用来增加s后缀
+function durationValue(value) {
     return value + 's';
 }
 
-
-/*
-    Uses an img element to create each leaf. "Leaves.css" implements two spin 
-    animations for the leaves: clockwiseSpin and counterclockwiseSpinAndFlip. This
-    function determines which of these spin animations should be applied to each leaf.
-    
-*/
-function createALeaf()
-{
-    /* Start by creating a wrapper div, and an empty img element */
+// 用img标签生成树叶。
+// 决定树叶旋转方向
+function createALeaf() {
+    // 用div包含img标签
     var leafDiv = document.createElement('div');
     var image = document.createElement('img');
     
-    /* Randomly choose a leaf image and assign it to the newly created element */
+    // 随机选择不同的图片作为树叶
     image.src = '../../files/images/' + randomInteger(1,4) + '.png';
     
+    // 定义div的位置、top固定，left随机
     leafDiv.style.top = "-100px";
-
-    /* Position the leaf at a random location along the screen */
     leafDiv.style.left = pixelValue(randomInteger(0, 500));
     
-    /* Randomly choose a spin animation */
+    // 随机选择旋转方向
     var spinAnimationName = (Math.random() < 0.5) ? 'clockwiseSpin' : 'counterclockwiseSpinAndFlip';
     
-    /* Set the -webkit-animation-name property with these values */
+    // 为div元素增加下落和渐隐的动画；为图片增加旋转的动画
     leafDiv.style.webkitAnimationName = 'fade, drop';
     image.style.webkitAnimationName = spinAnimationName;
     
-    /* Figure out a random duration for the fade and drop animations */
+    // 随机生成下落和渐隐动画的时间
     var fadeAndDropDuration = durationValue(randomFloat(5, 11));
-    
-    /* Figure out another random duration for the spin animation */
+    // 随机生成旋转的动画时间
     var spinDuration = durationValue(randomFloat(4, 8));
-    /* Set the -webkit-animation-duration property with these values */
+
+    // 通过webkitAnimationDuration来设置下落和渐隐的动画时间！
     leafDiv.style.webkitAnimationDuration = fadeAndDropDuration + ', ' + fadeAndDropDuration;
 
+    // 设置延时，让树叶在不同时刻后，开始旋转
     var leafDelay = durationValue(randomFloat(0, 5));
     leafDiv.style.webkitAnimationDelay = leafDelay + ', ' + leafDelay;
 
+    // 设置旋转的动画时间
     image.style.webkitAnimationDuration = spinDuration;
 
-    // add the <img> to the <div>
     leafDiv.appendChild(image);
-
-    /* Return this img element so it can be added to the document */
     return leafDiv;
 }
 
