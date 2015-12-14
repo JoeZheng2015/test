a30612f840b4c63982fdc536b7592fe453225647
JoeZheng2015 <18810544291@163.com>
2015-12-14 09:43:14 +0800

    修改Bubble.js

--- app/js/Bubble.js
+++ app/js/Bubble.js
@@ -7,21 +7,27 @@
  * {number}options.number 泡泡的数量，默认为画布宽度的一半
  * {string}options.img 在页面上图片的类，用来替换泡泡
  * {number}options.opacityC 泡泡的透明度系数，原始值为0.3，越大越透明
+ * {number}options.scale 泡泡大小的基数
+ * {number}options.speed 泡泡下降速度的基数
  */
 function Bubble(options) {
     this.el = typeof options === 'string' ? options : options.el;
-    this.canvas = document.querySelector(this.el);
-    this.canvas.width = options.width || 1000;
-    this.canvas.height = options.height || 300;
-    this.number = options.number || this.canvas.width * 0.5;
+    this.$el = $(this.el);
+    this.canvas = this.$el[0];
+    this.canvas.width = options.width || this.$el.parent().width();
+    this.canvas.height = options.height || this.$el.parent().height();
+    this.number = options.number || parseInt(this.canvas.width * 0.5);
     this.ctx = this.canvas.getContext('2d');
     this.circles = [];
     for(var i = 0; i < this.number; i++) {
         this.circles.push(new Circle({
-                ctx: this.ctx, 
+                ctx: this.ctx,
                 width: this.canvas.width,
+                height: this.canvas.height,
                 img: options.img,
-                opacityC: options.opacityC || 0.3
+                opacityC: options.opacityC || 0.3,
+                scale: options.scale || 0.1,
+                speed: options.speed || 0
             })
         );
     }
@@ -46,36 +52,33 @@ Bubble.prototype = {
     }
 };
 function Circle(option) {
-    this.ctx = option.ctx;
-    this.width = option.width;
-    this.img = option.img;
-    this.opacityC = option.opacityC;
+    this.option = option;
     this.init();
 }
 Circle.prototype = {
     constructor: Circle,
     init: function () {
-        this.x = Math.random() * this.width;
-        this.y = -Math.random() * 100;
-        this.opacity = 1 - Math.random() * this.opacityC;
-        this.scale = 0.1 + Math.random() * 0.3;
-        this.velocity = Math.random();
+        this.x = Math.random() * this.option.width;
+        this.y = -Math.random() * this.option.height;
+        this.opacity = 1 - Math.random() * this.option.opacityC;
+        this.scale =  this.option.scale + Math.random() * 0.5;
+        this.speed = this.option.speed + Math.random();
     },
     draw: function() {
-        if (this.opacity <= 0) {
+        if (this.opacity <= 0 || this.y >= this.option.height) {
             this.init();
         }
-        this.y += this.velocity;
+        this.y += this.speed;
         this.opacity -= 0.0005;
-        this.ctx.beginPath();
+        this.option.ctx.beginPath();
         if (this.img) {
             var img = document.querySelector('.snow');
-            this.ctx.drawImage(img, this.x, this.y, this.scale * 30, this.scale * 30);
+            this.option.ctx.drawImage(img, this.x, this.y, this.scale * 30, this.scale * 30);
         }
         else {
-            this.ctx.arc(this.x, this.y, this.scale * 10, 0, 2 * Math.PI, false);
-            this.ctx.fillStyle = 'rgba(255,255,255,'+ this.opacity+')';
+            this.option.ctx.arc(this.x, this.y, this.scale * 10, 0, 2 * Math.PI, false);
+            this.option.ctx.fillStyle = 'rgba(255,255,255,'+ this.opacity+')';
         }
-        this.ctx.fill();
+        this.option.ctx.fill();
     }
 };
