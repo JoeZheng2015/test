caaa96d1ed04ef1511c2fbd071527e820f9d9016
郑俊鑫 <zhengjunxin@wepiao.com>
2016-08-30 17:11:20 +0800

    增加demo

---
+++ app/js/throttling.js
@@ -0,0 +1,43 @@
+// 简单的防抖动函数
+// 多次调用，最终只执行一次
+function debounce(func, wait) {
+    // 定时器变量
+    var timeout;
+    return function() {
+        // 每次触发 scroll handler 时先清除定时器
+        clearTimeout(timeout);
+        // 指定 xx ms 后触发真正想进行的操作 handler
+        timeout = setTimeout(func, wait);
+    };
+};
+
+
+// 简单的节流函数
+// 多次调用，最终执行一次
+// 每个mustRun一定会执行一次
+function throttle(func, wait, mustRun) {
+    var timeout,
+        startTime = new Date();
+ 
+    return function() {
+        var context = this,
+            args = arguments,
+            curTime = new Date();
+ 
+        clearTimeout(timeout);
+        // 如果达到了规定的触发时间间隔，触发 handler
+        if(curTime - startTime >= mustRun){
+            func.apply(context,args);
+            startTime = curTime;
+        // 没达到触发间隔，重新设定定时器
+        }else{
+            timeout = setTimeout(func, wait);
+        }
+    };
+};
+// 实际想绑定在 scroll 事件上的 handler
+function realFunc(){
+    console.log("Success");
+}
+// 采用了节流函数
+window.addEventListener('scroll',throttle(realFunc,500,1000));
